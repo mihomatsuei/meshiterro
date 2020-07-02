6d5290e0a4f8eeccfe8f7b8253a690a9fe480cf0
松榮美帆 <>
2020-07-02 17:59:42 +0900

    [Finish]5-3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_055936) do
+ActiveRecord::Schema.define(version: 2020_07_02_075608) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
