9dc0d8dcca75ccbfb0aa106a2264cc55d6b734d8
松榮美帆 <>
2020-07-02 23:28:47 +0900

    [Finish]5-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -24,6 +24,12 @@ class PostImagesController < ApplicationController
   	@post_comment = PostComment.new
   end
 
+  def destroy
+  @post_image = PostImage.find(params[:id])
+  @post_image.destroy
+  redirect_to post_images_path
+  end
+
 end
 
 # 投稿データのストロングパラメータ
